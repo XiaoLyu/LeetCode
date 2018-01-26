d3ad9ee567d69ce08e9e55d263ac1198dc7870ee
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-01-26 17:09:11 -0600

    Update Move Zeroes.java

--- src/Move Zeroes.java
+++ src/Move Zeroes.java
@@ -7,3 +7,19 @@ Note:
 You must do this in-place without making a copy of the array.
 Minimize the total number of operations.
 */
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+}
+
+/* 还有别的解法， 复杂度 *／

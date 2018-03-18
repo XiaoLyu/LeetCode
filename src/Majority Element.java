ec09f97c2689ab23f174c50cc620ade9aec45419
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-03-18 15:07:04 +0800

    Update Majority Element.java

--- src/Majority Element.java
+++ src/Majority Element.java
@@ -3,4 +3,32 @@ Given an array of size n, find the majority element. The majority element is the
 
 You may assume that the array is non-empty and the majority element always exist in the array.
 
+HashMap存key-value
 */
+
+class Solution {
+    public int majorityElement(int[] nums) {
+       HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(!hashMap.containsKey(nums[i])){
+                hashMap.put(nums[i], 1);
+            }
+            else{
+                hashMap.put(nums[i], hashMap.get(nums[i]) + 1);
+            }
+        }
+        
+        int halftime = nums.length / 2;
+        int result = 0;
+        Iterator iter = hashMap.entrySet().iterator();
+        while(iter.hasNext()){
+            Map.Entry entry = (Map.Entry) iter.next();
+            if((Integer)entry.getValue() > halftime){
+                result = (Integer)entry.getKey();
+            }
+        }
+        
+        return result;
+	}
+}
+

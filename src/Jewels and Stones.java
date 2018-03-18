49067249f2e9fc0b4377f1e7ab0afe2a4a7fef91
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-03-18 18:41:15 +0800

    Update Jewels and Stones.java

--- src/Jewels and Stones.java
+++ src/Jewels and Stones.java
@@ -16,3 +16,27 @@ Note:
 S and J will consist of letters and have length at most 50.
 The characters in J are distinct.
 */
+
+/*
+ * Set代表集合元素无序，集合元素不可重复的集合
+ * Map代表一种由多个key-value组成的集合,key不能重复，没有顺序
+ * HashMap和Hashtable都实现了Map接口，不同在于HashMap是非synchronized的，可以接受null，而Hashtable则不行
+ * HashSet实现了Set接口，它不允许集合中有重复的值，HashMap实现了Map接口，Map接口对键值对进行映射
+ * HashSet拓展了HashMap，所以底层还是用到map存储，存储实现同map一致，HashMap存储键值，HashSet存储对象
+ */
+
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        Set set = new HashSet();
+		int result = 0;
+		for(char j: J.toCharArray()){
+			set.add(j);
+		}
+		for(char s: S.toCharArray()){
+			if(set.contains(s)){
+				result++;
+			}
+		}
+		return result;
+	}
+}

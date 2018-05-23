f59309cf18d29d3d0d5f6b1795091e6f2da2e7e5
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-05-23 15:41:36 +0800

    Update Best Time to Buy and Sell Stock.java

--- src/Best Time to Buy and Sell Stock.java
+++ src/Best Time to Buy and Sell Stock.java
@@ -17,3 +17,21 @@ Input: [7,6,4,3,1]
 Output: 0
 Explanation: In this case, no transaction is done, i.e. max profit = 0.
 */
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+		int maxProfit = 0;
+		
+		for(int i = 0; i < prices.length; i++){
+			if(prices[i] < min){
+				min = prices[i];
+			}
+			if((prices[i] - min) > maxProfit){
+				maxProfit = prices[i] - min;
+			}
+		}
+		
+		return maxProfit;
+    }
+}

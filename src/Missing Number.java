652066c1cf5faeb4c0bf75171fae683e7fd71e35
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-05-23 13:54:59 +0800

    Update Missing Number.java

--- src/Missing Number.java
+++ src/Missing Number.java
@@ -31,10 +31,10 @@ class Solution {
 class Solution {
     public int missingNumber(int[] nums) {
         int result = 0;
-				for(int i = 0; i < nums.length; i++){
-					result = result ^ nums[i] ^ i;
-				}
-				return result ^ nums.length;
+	for(int i = 0; i < nums.length; i++){
+		result = result ^ nums[i] ^ i;
+	}
+	return result ^ nums.length;
     }
 }
 

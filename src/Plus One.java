13fbb12199b82a73c7eaa2a771418e28d53abf8a
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-05-29 15:36:00 +0800

    Update Plus One.java

--- src/Plus One.java
+++ src/Plus One.java
@@ -16,3 +16,22 @@ Input: [4,3,2,1]
 Output: [4,3,2,2]
 Explanation: The array represents the integer 4321.
 */
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for(int i = digits.length-1; i >= 0; i--){
+			if(digits[i] < 9){
+				digits[i]++;
+				return digits;
+			}
+			else{
+				digits[i] = 0;
+			}
+		}
+		
+		int[] newNumber = new int[digits.length + 1];
+		newNumber[0] = 1;
+		
+		return newNumber;
+    }
+}

8c4e679f942e93b791ecf73c2d989c3c7563e952
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-05-29 16:46:58 +0800

    Update Two Sum.java

--- src/Two Sum.java
+++ src/Two Sum.java
@@ -10,3 +10,18 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 */
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+		for(int i = 0; i < nums.length-1; i++){
+			for(int j = i+1; j < nums.length; j++){
+				if(target == (nums[i] + nums[j])){
+					result[0] = i;
+					result[1] = j;
+				}
+			}
+		}
+		return result;
+    }
+}

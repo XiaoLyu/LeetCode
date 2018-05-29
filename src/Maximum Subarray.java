2cc95c6957f5bc5911f14101109a81a62754d961
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-05-29 14:37:14 +0800

    Update Maximum Subarray.java

--- src/Maximum Subarray.java
+++ src/Maximum Subarray.java
@@ -10,3 +10,26 @@ Follow up:
 
 If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
 */
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int[] tempArray = new int[nums.length];
+		tempArray[0] = nums[0];
+        for(int i = 1; i < nums.length; i++){
+        	if(nums[i] > (nums[i] + tempArray[i-1])){
+        		tempArray[i] = nums[i];
+        	}
+        	else{
+        		tempArray[i] = (nums[i] + tempArray[i-1]);
+        	}
+        }
+        
+        int max = tempArray[0];
+        for(int i = 1; i < tempArray.length; i++){
+        	if(tempArray[i] > max){
+        		max = tempArray[i];
+        	}
+        }
+        return max;
+    }
+}

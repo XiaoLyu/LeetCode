88b75ba20da32a0a8013ece096d7f54cf7247c05
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-05-29 14:25:11 +0800

    Update Pascal's Triangle.java

--- src/Pascal's Triangle.java
+++ src/Pascal's Triangle.java
@@ -16,3 +16,37 @@ Output:
  [1,4,6,4,1]
 ]
 */
+
+/*
+注意list的操作
+*/
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        
+        if(numRows > 0){
+        	// row = 1
+        	result.add(new ArrayList<Integer>());
+        	result.get(0).add(1);
+        	
+        	// row = 2, 3...
+        	for(int rowNum = 1; rowNum < numRows; rowNum++){
+        		List<Integer> row = new ArrayList<>();
+        		List<Integer> pre = result.get(rowNum-1);
+        		
+        		row.add(1);
+        		
+        		for(int j = 1; j < rowNum; j++){
+        			row.add(pre.get(j-1) + pre.get(j));
+        		}
+        		
+        		row.add(1);
+        		
+        		result.add(row);
+        	}
+        	
+	    }
+        return result; 
+    }
+}

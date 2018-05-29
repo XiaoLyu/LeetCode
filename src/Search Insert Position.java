fb02911633a57d301dafb070bfa9ea0a06b78bf4
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-05-29 15:18:40 +0800

    Update Search Insert Position.java

--- src/Search Insert Position.java
+++ src/Search Insert Position.java
@@ -21,3 +21,45 @@ Input: [1,3,5,6], 0
 Output: 0
 */
 
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        int result = 0;
+        
+        if(target < nums[0]){
+        	result = 0;
+        }
+        else if(target > nums[right]){
+        	result = right + 1;
+        }
+        else{
+        	while(left < right){
+            	int middle = (left + right) / 2;
+            	if(nums[middle] > target){
+            		right = middle-1;
+            		result = middle;
+            	}
+            	else if(nums[middle] == target){
+            		result = middle;
+            		left = middle;
+            		right = middle;
+            	}
+            	else if(nums[middle] < target){
+            		left = middle + 1;
+            		result = middle;
+            	}
+            }
+            if(left == right){
+            	if(nums[left] >= target){
+            		result = left;
+            	}
+            	else{
+            		result = left + 1;
+            	}
+            	
+            }
+        }
+        return result;
+    }
+}

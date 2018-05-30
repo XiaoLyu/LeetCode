9652311a57f095fbc516b371aca75e7a1ae3192c
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-05-30 10:45:07 +0800

    Update Contains Duplicate II.java

--- src/Contains Duplicate II.java
+++ src/Contains Duplicate II.java
@@ -14,3 +14,33 @@ Example 3:
 Input: [1,2,1], k = 0
 Output: false
 */
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
+		boolean result = false;
+		
+		for(int i = 0; i < nums.length; i++){
+			if(!map.containsKey(nums[i])){
+				map.put(nums[i], new ArrayList<Integer>());
+				map.get(nums[i]).add(i);
+			}
+			else{
+				map.get(nums[i]).add(i);
+			}
+		}
+		
+		for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {  
+			  
+		    int length = entry.getValue().size();
+		    if(length > 1){
+		    	for(int i = 1; i < length; i++){
+		    		if(entry.getValue().get(i) - entry.getValue().get(i-1) <= k){
+		    			result = true;
+		    		}
+		    	}
+		    }
+		}  
+		return result;
+    }
+}

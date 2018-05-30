2b4a26af149505bf0445ea96c68a7939d9967dc6
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-05-30 15:19:17 +0800

    Update Third Maximum Number.java

--- src/Third Maximum Number.java
+++ src/Third Maximum Number.java
@@ -22,3 +22,43 @@ Explanation: Note that the third maximum here means the third maximum distinct n
 Both numbers with value 2 are both considered as second maximum.
 */
 
+class Solution {
+    public int thirdMax(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+    	for(int i = 0; i < nums.length; i++){
+    		set.add(nums[i]);
+    	}
+    	
+    	int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+    	
+    	  
+    	
+    	if(set.size() < 3){
+    		// return max
+    		for (Integer item : set) {
+        		if(item > max1){
+        			max1 = item;
+        		}
+        	}
+    		return max1;
+    	}
+    	else{
+    		// return third max
+    		for (Integer item : set) {
+    			if(item > max1){
+    				max3 = max2;
+    				max2 = max1;
+    				max1 = item;
+    			}
+    			else if(item < max1 && item > max2){
+    				max3 = max2;
+    				max2 = item;
+    			}
+    			else if(item < max2 && item > max3){
+    				max3 = item;
+    			}
+    		}
+    		return max3;
+    	}
+    }
+}

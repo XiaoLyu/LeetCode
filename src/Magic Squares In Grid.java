58ef669c3958af48209449870b5c442a898ba773
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-05-30 10:13:55 +0800

    Update Magic Squares In Grid.java

--- src/Magic Squares In Grid.java
+++ src/Magic Squares In Grid.java
@@ -29,3 +29,83 @@ Note:
 1 <= grid[0].length <= 10
 0 <= grid[i][j] <= 15
 */
+
+class Solution {
+    public int numMagicSquaresInside(int[][] grid) {
+        int result = 0;
+		if(grid.length >=3 && grid[0].length >= 3){
+			int x = grid.length - 3; // x 最后的index
+			int y = grid[0].length - 3; // y 最后的index
+			
+			for(int i = 0; i <= x; i++){
+				for(int j = 0; j <= y; j++){
+					if(magic(grid, i, j)){
+						result++;
+					}
+				}
+			}
+		}
+		return result;
+	}
+	
+	public static boolean magic(int[][] grid, int r, int c){
+		boolean flag = true;
+		if(uniq(grid, r, c) && rc(grid, r, c) && diagonals(grid, r, c)){
+			flag = true;
+		}
+		else{
+			flag = false;
+		}
+		return flag;
+	}
+	
+	public static boolean uniq(int[][] grid, int r, int c){
+		List<Integer> list = new ArrayList<Integer>();
+		boolean flag = true;
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 3; j++){
+				if(!list.contains(grid[r+i][c+j]) && grid[r+i][c+j] >= 0 && grid[r+i][c+j] <= 9){
+					list.add(grid[r+i][c+j]);
+				}
+				else{
+					flag = false;
+				}
+			}
+		}
+		return flag;
+	}
+	
+	public static boolean rc(int[][] grid, int r, int c){
+		boolean flag = true;
+		
+		for(int i = 0; i < 3; i++){
+			int Rsum = 0;
+			int Csum = 0;
+			for(int j = 0; j < 3; j++){
+				Rsum = Rsum + grid[r+i][c+j];
+				Csum = Csum + grid[r+j][c+i];
+			}
+			if(Rsum != 15 || Csum != 15){
+				flag = false;
+			}
+		}
+		return flag;
+	}
+	
+	public static boolean diagonals(int[][] grid, int r, int c){
+		boolean flag = true;
+		int left2right = 0;
+		int right2left = 0;
+		
+		for(int i = 0; i < 3; i++){
+			left2right = left2right + grid[r+i][c+i];
+			right2left = right2left + grid[r+i][c+2-i];
+		}
+		
+		if(left2right != 15 || right2left != 15){
+			flag = false;
+		}
+		
+		return flag;
+	}
+}

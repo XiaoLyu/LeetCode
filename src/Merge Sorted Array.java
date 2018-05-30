da54d7c9022141562e97ba7070871ec4ba6dcdb6
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-05-30 11:17:21 +0800

    Update Merge Sorted Array.java

--- src/Merge Sorted Array.java
+++ src/Merge Sorted Array.java
@@ -13,3 +13,36 @@ nums2 = [2,5,6],       n = 3
 
 Output: [1,2,2,3,5,6]
 */
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index1 = m-1;
+		int index2 = n-1;
+		int index = m+n-1;
+		
+		if(index1 == -1){
+			for(int i = 0; i < n; i++){
+				nums1[i] = nums2[i];
+			}
+		}
+		else{
+			while(index1 >= 0 && index2 >= 0){
+				if(nums1[index1] >= nums2[index2]){
+					nums1[index] = nums1[index1];
+					index1--;
+					index--;
+				}
+				else{
+					nums1[index] = nums2[index2];
+					index2--;
+					index--;
+				}
+			}
+			while(index2 >= 0){
+				nums1[index] = nums2[index2];
+				index--;
+				index2--;
+			}
+		}
+    }
+}

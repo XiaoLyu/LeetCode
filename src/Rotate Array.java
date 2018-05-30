8e5f420d4a81f39798a654cad98dc3c7938fa5a0
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-05-30 15:51:42 +0800

    Update Rotate Array.java

--- src/Rotate Array.java
+++ src/Rotate Array.java
@@ -21,3 +21,22 @@ Note:
 Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
 Could you do it in-place with O(1) extra space?
 */
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+    	reverse(nums, 0, nums.length-1);
+    	reverse(nums, 0, k-1);
+    	reverse(nums, k, nums.length-1);
+    }
+    
+    public void reverse(int[] nums, int start, int end){
+    	while(start < end){
+    		int temp = nums[start];
+    		nums[start] = nums[end];
+    		nums[end] = temp;
+    		start++;
+    		end--;
+    	}
+    }
+}

5a4fd142fa2a0de75485d4fb58ca26ee7d8504cf
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-05-31 08:51:06 +0800

    Update Single Number.java

--- src/Single Number.java
+++ src/Single Number.java
@@ -14,3 +14,14 @@ Example 2:
 Input: [4,1,2,1,2]
 Output: 4
 */
+
+class Solution {
+    public int singleNumber(int[] nums) {
+         int result = 0;
+		 for(int i = 0; i < nums.length; i++)
+		 {
+			 result = result ^ nums[i];
+		 }
+		 return result;
+    }
+}

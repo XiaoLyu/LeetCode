68e81114d7bf3212016d04d0d4a3c5fb94de00a0
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-05-31 08:19:51 +0800

    Update Keyboard Row.java

--- src/Keyboard Row.java
+++ src/Keyboard Row.java
@@ -12,3 +12,38 @@ Note:
 You may use one character in the keyboard more than once.
 You may assume the input string will only contain letters of alphabet.
 */
+
+class Solution {
+    public String[] findWords(String[] words) {
+        String[] line = {"qwertyuiop", "asdfghjkl", "zxcvbnm"};
+		ArrayList<String> result = new ArrayList<String>();
+		
+		Map<Character, Integer> dictionary = new HashMap<Character, Integer>();
+		
+		for(int i = 0; i < 3; i++){
+			for(char c: line[i].toCharArray()){
+				dictionary.put(c, i);
+			}
+		}
+		
+		for(String c: words){
+			int flag = 1;
+			for(int i = 0 ; i < c.length(); i++){
+				char cha = c.toLowerCase().charAt(i);
+				
+				char firstcha = c.toLowerCase().charAt(0);
+				int index1 = dictionary.get(firstcha);
+				
+				if(dictionary.get(cha) != index1){
+					flag = -1;
+					break;
+				}	
+			}
+			if(flag == 1){
+				result.add(c);
+			}
+			
+		}
+		return result.toArray(new String[0]);
+    }
+}

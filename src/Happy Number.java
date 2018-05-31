6f91721177c5ac15e1a03c914189831ff00670b8
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-05-31 14:53:17 +0800

    Update Happy Number.java

--- src/Happy Number.java
+++ src/Happy Number.java
@@ -13,3 +13,24 @@ Explanation:
 62 + 82 = 100
 12 + 02 + 02 = 1
 */
+
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> result = new HashSet<Integer>();
+        while(result.add(n)){
+        	int sum = 0;
+        	while(n > 0){
+        		int remain = n % 10;
+        		sum = sum + remain * remain;
+        		n = n/10;
+        	}
+        	if(sum == 1){
+        		return true;
+        	}
+        	else{
+        		n = sum;
+        	}
+        }
+        return false;
+    }
+}

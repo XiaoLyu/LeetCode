b4d64ed1256be2861cd61560225175cffd8d0386
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-05-31 13:51:02 +0800

    Update Longest Palindrome.java

--- src/Longest Palindrome.java
+++ src/Longest Palindrome.java
@@ -17,3 +17,37 @@ Output:
 Explanation:
 One longest palindrome that can be built is "dccaccd", whose length is 7.
 */
+
+class Solution {
+    public int longestPalindrome(String s) {
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        
+    	for(int i = 0; i < s.length(); i++){
+    		if(!map.containsKey(s.charAt(i))){
+    			map.put(s.charAt(i), 1);
+    		}
+    		else{
+    			map.put(s.charAt(i), map.get(s.charAt(i))+1);
+    		}
+    	}
+    	
+    	int result = 0;
+    	int remain = Integer.MIN_VALUE;
+    	
+    	for(Map.Entry<Character, Integer> entry : map.entrySet()){
+    		if(entry.getValue() % 2 == 0){
+    			result = result + entry.getValue();
+    		}
+    		else{
+                result = result + entry.getValue() - 1;
+    			if(entry.getValue() >= remain){
+    				remain = entry.getValue();
+    			}
+    		}
+    	}
+    	if(remain != Integer.MIN_VALUE){
+    		result = result + 1;
+    	}
+    	return result;
+    }
+}

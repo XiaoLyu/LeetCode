0820116cc7e3fd5d7b0e13c4db8d4a74abad9481
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-05-31 09:37:28 +0800

    Update First Unique Character in a String.java

--- src/First Unique Character in a String.java
+++ src/First Unique Character in a String.java
@@ -11,3 +11,37 @@ return 2.
 Note: You may assume the string contain only lowercase letters.
 
 */
+
+class Solution {
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+    	
+        for(int i = 0; i < s.length(); i++){
+        	if(!map.containsKey(s.charAt(i))){
+        		map.put(s.charAt(i), 1);
+        	}
+        	else{
+        		map.put(s.charAt(i), map.get(s.charAt(i))+1);
+        	}
+        }
+        
+        int min = Integer.MAX_VALUE;
+        if(map.size() == 0 || map == null){
+        	min = -1;
+        }
+        else{
+        	for(Map.Entry<Character, Integer> entry : map.entrySet()){
+            	if(entry.getValue() == 1){
+            		int index = s.indexOf(entry.getKey());
+            		if(index < min){
+            			min = index;
+            		}
+            	}
+        	}
+        }
+    	if(min == Integer.MAX_VALUE){
+        	min = -1;
+        }
+    	return min;
+    }
+}

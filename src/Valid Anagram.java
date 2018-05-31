a7b8952562b06f78e2118f2b4fe0425ff9a394d4
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-05-31 09:08:12 +0800

    Update Valid Anagram.java

--- src/Valid Anagram.java
+++ src/Valid Anagram.java
@@ -15,3 +15,34 @@ You may assume the string contains only lowercase alphabets.
 Follow up:
 What if the inputs contain unicode characters? How would you adapt your solution to such case?
 */
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        for(int i = 0; i < s.length(); i++){
+        	if(!map.containsKey(s.charAt(i))){
+        		map.put(s.charAt(i), 1);
+        	}
+        	else{
+        		map.put(s.charAt(i), map.get(s.charAt(i))+1);
+        	}
+        }
+        
+        for(int i = 0; i < t.length(); i++){
+        	if(map.containsKey(t.charAt(i))){
+        		map.put(t.charAt(i), map.get(t.charAt(i))-1);
+        	}
+        	else{
+        		return false;
+        	}
+        }
+        
+        for(Map.Entry<Character, Integer> entry : map.entrySet()){
+        	if(entry.getValue() != 0){
+        		return false;
+        	}
+        }
+        
+        return true;
+    }
+}

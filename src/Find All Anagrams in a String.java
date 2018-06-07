adc9403d80c4c7c1e31fb3b058bdfe379db7c7f3
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-06-07 17:28:39 +0800

    Update Find All Anagrams in a String.java

--- src/Find All Anagrams in a String.java
+++ src/Find All Anagrams in a String.java
@@ -29,3 +29,51 @@ The substring with start index = 0 is "ab", which is an anagram of "ab".
 The substring with start index = 1 is "ba", which is an anagram of "ab".
 The substring with start index = 2 is "ab", which is an anagram of "ab".
 */
+
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<Integer>();
+        Map<Character, Integer> map = new HashMap<>();
+        
+        for(Character c: p.toCharArray()){
+        	if(map.containsKey(c)){
+        		map.put(c, map.get(c) + 1);
+        	}
+        	else{
+        		map.put(c, 1);
+        	}
+        }
+        
+        int begin = 0, end = 0;
+        int counter = map.size();
+        
+        while(end < s.length()){
+        	Character c = s.charAt(end);
+        	if(map.containsKey(c)){
+        		map.put(c, map.get(c)-1);
+        		if(map.get(c) == 0){
+            		counter--;
+            	}
+        	}
+        	
+        	end++;
+        	
+        	// counter !!!这个地方超级厉害！！！
+        	while(counter == 0){
+        		if(end - begin == p.length()){
+        			result.add(begin);
+        		}
+        		
+        		Character t = s.charAt(begin);
+        		if(map.containsKey(t)){
+        			map.put(t, map.get(t) + 1);
+        			if(map.get(t) > 0){
+        				counter++;
+        			}
+        		}
+        		begin++;
+        	}
+        }
+        return result;
+    }
+}

d73dc0cd18e6da453c33feefc1bdb25a7e700822
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-06-08 16:45:35 +0800

    Update Linked List Cycle.java

--- src/Linked List Cycle.java
+++ src/Linked List Cycle.java
@@ -4,3 +4,32 @@ Given a linked list, determine if it has a cycle in it.
 Follow up:
 Can you solve it without using extra space?
 */
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+//1. Hash Table Solution
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> nodesSeen = new HashSet<>();
+        while(head != null){
+        	if(nodesSeen.contains(head)){
+        		return true;
+        	}
+        	else{
+        		nodesSeen.add(head);
+        	}
+        	head = head.next;
+        }
+        return false;
+    }
+}

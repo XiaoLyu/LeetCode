8774d6edf1996e54cecdc744a9ac2ad9512b66d3
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-06-08 17:09:16 +0800

    Update Linked List Cycle.java

--- src/Linked List Cycle.java
+++ src/Linked List Cycle.java
@@ -33,3 +33,23 @@ public class Solution {
         return false;
     }
 }
+
+//2. Two Pointers: a fast pointer and a slow pointer
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null) return false;
+        ListNode fast = head.next;
+        ListNode slow = head;
+        
+        while(fast != slow){
+        	if(fast == null || fast.next == null){
+        		return false;
+        	}
+        	else{
+        		fast = fast.next.next;
+        		slow = slow.next;
+        	}
+        }
+        return true;
+    }
+}

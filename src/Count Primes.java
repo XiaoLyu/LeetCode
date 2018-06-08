a2757f6d527e11297456d30bcb1a18895da92b45
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-06-08 09:00:49 +0800

    Update Count Primes.java

--- src/Count Primes.java
+++ src/Count Primes.java
@@ -7,3 +7,19 @@ Input: 10
 Output: 4
 Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
 */
+
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] result = new boolean[n];
+        int count = 0;
+        for(int i = 2; i < n; i++){
+        	if(result[i-1] == false){
+        		count++;
+        	}
+        	for(int j = 2; i*j <= n; j++){
+        		result[i*j - 1] = true;
+        	}
+        }
+        return count;
+    }
+}

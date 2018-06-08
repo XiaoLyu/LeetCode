9c2c956342450b2217353628032538b151220a34
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-06-08 15:30:59 +0800

    Update Merge Two Sorted Lists.java

--- src/Merge Two Sorted Lists.java
+++ src/Merge Two Sorted Lists.java
@@ -6,3 +6,41 @@ Example:
 Input: 1->2->4, 1->3->4
 Output: 1->1->2->3->4->4
 */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(-1);
+    	ListNode current = head;
+    	
+    	while(l1 != null || l2 != null){
+    		if(l1 != null && l2 != null){
+    			if(l1.val <= l2.val){
+    				current.next = l1;
+    				l1 = l1.next;
+    			}
+    			else{
+    				current.next = l2;
+    				l2 = l2.next;
+    			}
+    		}
+    		else if(l1 != null){
+    			current.next = l1;
+    			l1 = l1.next;
+    		}
+    		else if(l2 != null){
+    			current.next = l2;
+    			l2 = l2.next;
+    		}
+    		current = current.next;
+    	}
+    	return head.next;
+    }
+}

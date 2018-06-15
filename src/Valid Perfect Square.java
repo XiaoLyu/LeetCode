8879fae456ca260248e749a017a03ce3ef2c6374
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-06-15 17:23:07 +0800

    Update Valid Perfect Square.java

--- src/Valid Perfect Square.java
+++ src/Valid Perfect Square.java
@@ -12,3 +12,30 @@ Example 2:
 Input: 14
 Returns: False
 */
+// Brute Force
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num==1 || num==4)
+	 	   return true;
+	     int mid=num/2;
+	     for(int i=3;i<=mid;i++)
+	         if(i*i==num)
+	             return true;
+	     return false;
+    }
+}
+
+// 1+3+5+... = N^2
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int i = 1;
+	    while(num > 0){
+	    	num -= i;
+	    	i += 2;
+	    }
+	    return num == 0;
+    }
+}
+
+// Newton's method 牛顿迭代法
+// O(1) time Newton's method进阶版：fast inverse square root implementation from Quake III Arena

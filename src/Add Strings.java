11c39d429f8026bb0631f5004c9613eda122c7ee
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-06-15 14:06:17 +0800

    Update Add Strings.java

--- src/Add Strings.java
+++ src/Add Strings.java
@@ -8,3 +8,18 @@ Both num1 and num2 contains only digits 0-9.
 Both num1 and num2 does not contain any leading zero.
 You must not use any built-in BigInteger library or convert the inputs to integer directly.
 */
+
+class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder result = new StringBuilder();
+    	int carry = 0;
+    	for(int i = num1.length()-1, j = num2.length()-1; i>=0 || j>=0 || carry == 1; i--, j--){
+    		int x = i < 0 ? 0: num1.charAt(i)-'0';
+    		int y = j < 0 ? 0: num2.charAt(j)-'0';
+    		int sum = (x + y + carry)%10;
+    		result.append(sum);
+    		carry = (x + y + carry)/10;
+    	}
+    	return result.reverse().toString();
+    }
+}

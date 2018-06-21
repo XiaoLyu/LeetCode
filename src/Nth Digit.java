ee5172b6cbfa68691195b2733875f2043739fefd
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-06-21 15:53:27 +0800

    Update Nth Digit.java

--- src/Nth Digit.java
+++ src/Nth Digit.java
@@ -22,3 +22,24 @@ Output:
 Explanation:
 The 11th digit of the sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ... is a 0, which is part of the number 10.
 */
+
+// 注意溢出（long）
+class Solution {
+    public int findNthDigit(int n) {
+      int len = 1;
+    	long cou = 9;
+    	int remain = n;
+    	int start = 1;
+    	
+    	while(remain - len * cou > 0){
+    		remain = (int)(remain - len * cou);
+    		len++;
+    		cou = cou * 10;
+    		start = start * 10;
+    	}
+    	
+    	start = start + (remain-1) / len;
+    	String s = Integer.toString(start);
+    	return s.charAt((remain-1)%len)-'0';
+    }
+}

5e000eff4fac6f8b2138ace8c3f029d859f03523
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-06-22 10:05:24 +0800

    Update Reverse Vowels of a String.java

--- src/Reverse Vowels of a String.java
+++ src/Reverse Vowels of a String.java
@@ -10,3 +10,31 @@ Given s = "leetcode", return "leotcede".
 Note:
 The vowels does not include the letter "y".
 */
+
+class Solution {
+    public String reverseVowels(String s) {
+        if(s == null || s.length() == 0)	return s;
+    	
+    	String vowels = "aeiouAEIOU";
+    	int start = 0;
+    	int end = s.length() -1;
+    	char[] chars = s.toCharArray();
+    	
+    	while(start < end){
+    		while(start < end && !vowels.contains(chars[start]+"")){
+    			start++;
+    		}
+    		while(start < end && !vowels.contains(chars[end]+"")){
+    			end--;
+    		}
+    		
+    		char temp = chars[start];
+    		chars[start] = chars[end];
+    		chars[end] = temp;
+            
+            start++;
+    		end--;
+    	}
+    	return String.valueOf(chars);
+    }
+}

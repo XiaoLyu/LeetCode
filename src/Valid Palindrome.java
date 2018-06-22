8cdb32214984db99fe6373df857695344fe6c7cf
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-06-22 16:28:38 +0800

    Update Valid Palindrome.java

--- src/Valid Palindrome.java
+++ src/Valid Palindrome.java
@@ -12,3 +12,39 @@ Example 2:
 Input: "race a car"
 Output: false
 */
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        s = s.trim();
+        
+        String alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789";
+        int start = 0;
+        int end = s.length()-1;
+        
+        while(start < end){
+        	char sChar = s.charAt(start);
+        	char eChar = s.charAt(end);
+        	
+        	// If they are all alphanumeric, equal?
+        	if(alphanumeric.contains(sChar+"") && alphanumeric.contains(eChar+"")){
+        		if(sChar != eChar){
+        			return false;
+        		}
+        		else{
+        			start++;
+            		end--;
+        		}
+        	}
+        	else{
+        		if(!alphanumeric.contains(sChar+"")){
+        			start++;
+        		}
+        		if(!alphanumeric.contains(eChar+"")){
+        			end--;
+        		}
+        	}
+        }
+        return true;
+    }
+}

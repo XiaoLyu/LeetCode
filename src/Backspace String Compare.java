7b3812a25ba1210d61dbae04021b1bcb9fe7612d
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-06-22 09:06:31 +0800

    Update Backspace String Compare.java

--- src/Backspace String Compare.java
+++ src/Backspace String Compare.java
@@ -77,3 +77,25 @@ class Solution {
         return true;
     }
 }
+
+// Solution 1: Build String
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        return build(S).equals(build(T));
+    }
+
+    public String build(String S) {
+        Stack<Character> ans = new Stack();
+        for (char c: S.toCharArray()) {
+            if (c != '#')
+                ans.push(c);
+            else if (!ans.empty())
+                ans.pop();
+        }
+        return String.valueOf(ans);
+    }
+}
+
+// Solution 2: Two Pointer
+// reverse two strings
+

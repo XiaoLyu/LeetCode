49ffb95780d9164812f71d110233d6f04425bc24
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-06-22 16:06:19 +0800

    Update Implement strStr().java

--- src/Implement strStr().java
+++ src/Implement strStr().java
@@ -17,3 +17,37 @@ What should we return when needle is an empty string? This is a great question t
 
 For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
 */
+
+//Brute-Force
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle != null && haystack != null && haystack.length() >= needle.length() && needle.length() > 0){
+    		int start = 0;
+    		int count = 0;
+    		while(haystack.length() - start >= needle.length()){
+    			if(haystack.charAt(count+start) == needle.charAt(count)){
+    				count++;
+    			}
+    			else{
+    				count = 0;
+    				start++;
+    			}
+    			if(count == needle.length()){
+    				return start;
+    			}
+    		}
+    	}
+        else if(needle.length() == 0){
+    		return 0;
+    	}
+    	return -1;
+    }
+}
+
+// KMP: 基于BF，不符合的时候移位不一样
+
+// BM: Boyer-Moore,从needle右至左进行比较
+
+// Sunday: 与BM类似。从左至右进行比较
+
+// 438?

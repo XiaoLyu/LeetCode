4cc7e0248ae873e2a428f893c131cfa0f7fbd2ab
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-06-22 17:08:05 +0800

    Update Reverse Words in a String III.java

--- src/Reverse Words in a String III.java
+++ src/Reverse Words in a String III.java
@@ -6,3 +6,26 @@ Input: "Let's take LeetCode contest"
 Output: "s'teL ekat edoCteeL tsetnoc"
 Note: In the string, each word is separated by single space and there will not be any extra space in the string.
 */
+
+class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.split(" ");
+		StringBuilder result = new StringBuilder();
+		// reverse every word
+		for(String word: words){
+			char[] wordChar = word.toCharArray();
+			int start = 0;
+			int end = word.length()-1;
+			while(start < end){
+				char temp = wordChar[start];
+				wordChar[start] = wordChar[end];
+				wordChar[end] = temp;
+				start ++;
+				end--;
+			}
+			result.append(String.valueOf(wordChar));
+			result.append(" ");
+		}
+		return result.substring(0, result.length()-1);
+    }
+}

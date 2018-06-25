fdae065585b76d4a310fb093c300faf1371a404b
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-06-25 10:34:19 +0800

    Update Rotated Digits.java

--- src/Rotated Digits.java
+++ src/Rotated Digits.java
@@ -15,3 +15,24 @@ Note:
 
 N  will be in range [1, 10000].
 */
+
+// 一个数中只要包含2，5， 6，9并不含3，4，7的算合格
+class Solution {
+    public int rotatedDigits(int N) {
+        int count = 0;
+    	for(int i = 0; i <=N; i++){
+    		if(isValid(i)) count++;
+    	}
+    	return count;
+    }
+    
+    public boolean isValid(int N){
+    	boolean valid = false;
+        while(N > 0){
+        	if(N % 10 == 2 || N % 10 == 5 || N % 10 == 6 || N % 10 == 9)	valid = true;
+        	if(N % 10 == 3 || N % 10 == 4 || N % 10 == 7) return false;
+        	N = N / 10;
+        }
+        return valid;
+    }
+}

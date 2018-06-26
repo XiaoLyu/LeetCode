778edcdcd73b34199349177a8dbe48ab1d45f2c8
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-06-26 11:40:42 +0800

    Update Student Attendance Record I.java

--- src/Student Attendance Record I.java
+++ src/Student Attendance Record I.java
@@ -14,3 +14,31 @@ Example 2:
 Input: "PPALLL"
 Output: False
 */
+
+class Solution {
+    public boolean checkRecord(String s) {
+        int[] records = new int[2]; // [0]:A	[1]:L
+        for(int i = 0; i < s.length(); i++){
+        	Character c = s.charAt(i);
+        	if(c.equals('L')){
+        		records[1]++;
+        		if(records[1] > 2){
+        			return false;
+        		}
+        	}
+        	if(c.equals('A')){
+        		records[0]++;
+        		records[1] = 0;
+        	}
+            if(c.equals('P')){
+        		records[1] = 0;
+        	}
+        }
+        if(records[0] > 1){
+        	return false;
+        }
+        return true;
+    }
+}
+
+// contains或者RE

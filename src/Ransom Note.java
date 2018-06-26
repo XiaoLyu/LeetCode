119608020274368ee60306e6baf681bd59032f27
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-06-26 11:16:45 +0800

    Update Ransom Note.java

--- src/Ransom Note.java
+++ src/Ransom Note.java
@@ -10,3 +10,25 @@ canConstruct("a", "b") -> false
 canConstruct("aa", "ab") -> false
 canConstruct("aa", "aab") -> true
 */
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+         int[] ransom = new int[26];
+        int[] mag = new int[26];
+        
+        for(Character c: ransomNote.toCharArray()){
+        	ransom[c-'a']++;
+        }
+        
+        for(Character c: magazine.toCharArray()){
+        	mag[c -'a']++;
+        }
+        
+        for(int i = 0; i < 26; i++){
+        	if(mag[i] < ransom[i]){
+        		return false;
+        	}
+        }
+        return true;
+    }
+}

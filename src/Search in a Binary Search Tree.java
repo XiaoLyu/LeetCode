718c69f1560c9fe8249d75e34d1564e782993f88
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-11-18 23:00:47 +0800

    Update Search in a Binary Search Tree.java

--- src/Search in a Binary Search Tree.java
+++ src/Search in a Binary Search Tree.java
@@ -30,6 +30,7 @@ Note that an empty tree is represented by NULL, therefore you would see the expe
  *     TreeNode(int x) { val = x; }
  * }
  */
+
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
        TreeNode result = root;

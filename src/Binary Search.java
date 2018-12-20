198e184c38589c4b370992276e55bf70dce300fc
XiaoLyu <32341287+XiaoLyu@users.noreply.github.com>
2018-12-20 09:29:44 +0800

    Update Binary Search.java

--- src/Binary Search.java
+++ src/Binary Search.java
@@ -41,3 +41,25 @@ class Solution {
         return result;
     }
 }
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int result = -1;
+        
+        while(low <= high){
+        	int mid = low + (high - low)/2;
+        	if(nums[mid] == target){
+        		return mid;
+        	}
+        	if(nums[mid] > target){
+        		high = mid - 1;
+        	}
+        	else{
+        		low = mid + 1;
+        	}
+        }
+        return result;
+    }
+}
